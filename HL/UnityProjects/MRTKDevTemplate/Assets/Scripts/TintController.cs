214fcaa9aee61bb827dafb2e68fe1c8983ea7e1e
Marc Fischer <57159035+mrcfschr@users.noreply.github.com>
2024-07-19 09:26:27 +0200

    paths are blue, it's possible to toggle from red green to blue yellow

--- HL/UnityProjects/MRTKDevTemplate/Assets/Scripts/TintController.cs
+++ HL/UnityProjects/MRTKDevTemplate/Assets/Scripts/TintController.cs
@@ -4,14 +4,31 @@ public class TintController : MonoBehaviour
 {
     public float tintValue; 
     private Renderer modelRenderer;
+    public bool switchColor = false;
+    public Color startColor = Color.red;
+    public Color endColor = Color.green;
 
     public void Start()
     {
         this.modelRenderer = GetComponent<Renderer>();
     }
+
+    public void ToggleColor() {
+        switchColor = !switchColor;
+    }
     void Update()
     {
-        Color newColor = Color.Lerp(Color.red, Color.green, tintValue);
+        if (switchColor)
+        {
+            startColor = Color.blue;
+            endColor = Color.yellow;
+        }
+        else {
+            startColor = Color.red;
+            endColor = Color.green;
+
+        }
+        Color newColor = Color.Lerp(startColor, endColor, tintValue);
         if (modelRenderer != null)
         {
             modelRenderer.material.color = newColor;
